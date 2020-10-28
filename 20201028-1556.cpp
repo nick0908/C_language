91d96c4a6490301ade254d3fb3db16cfa456213e
nick0908 <72485384+nick0908@users.noreply.github.com>
2020-10-28 16:08:48 +0900

    Add files via upload

---
+++ 20201028-1556.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int n;
+
+long long int f(int a)
+{
+	long long int result=1;
+	for (int i=1;i<=a;i++)
+	{
+		result=result*i;
+	}
+	return result;
+}
+
+int main(void)
+{
+	scanf ("%d", &n);
+	printf ("%lld", f(n));
+		return 0;
+}
